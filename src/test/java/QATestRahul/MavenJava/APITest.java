fd306084155ec6df575d53847ea1b125b89c6f3e
abhi7625 <abhishekt965@gmail.com>
2020-11-19 21:00:22 +0530

    first Commit

---
+++ src/test/java/QATestRahul/MavenJava/APITest.java
@@ -0,0 +1,32 @@
+package QATestRahul.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+@Test  
+public void postJIRA()
+{
+	System.out.println("postJIRA");	
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+}
+@Test   
+public void twitterAPI()
+{
+	System.out.println("twitterAPI");
+}
+@Test
+public void postJIRA2()
+{
+	System.out.println("postJIRA");	
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+	System.out.println("postJIRA");
+
+}
+
+}
---
+++ src/test/java/QATestRahul/MavenJava/appiumTest2.java
@@ -0,0 +1,19 @@
+package QATestRahul.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+@Test
+public void nativeApp()
+{
+	System.out.println("nativeApp");
+}
+@Test
+public void IOSapp()
+{
+	System.out.println("IOSapp");
+	System.out.println("IOSapp");
+	System.out.println("IOSapp");
+
+}
+}
